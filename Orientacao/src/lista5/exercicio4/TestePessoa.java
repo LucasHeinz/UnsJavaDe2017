011c9efcc356d38ea7aad9b2408e95ca57cca8d9
Noturno <Noturno@192.168.7.189>
2017-08-17 21:34:06 -0300

    COMMIT DE UMA COISA QUE FALTOU ANTERIORMENTE, PQ DEPOIS NÃO DAVA :D

--- Orientacao/src/lista5/exercicio4/TestePessoa.java
+++ Orientacao/src/lista5/exercicio4/TestePessoa.java
@@ -4,7 +4,7 @@ public class TestePessoa {
 
 	public static void main(String[] args) {
 
-		Pessoa pessoa = new Pessoa("Jaílson", 29, 2, 2016);
+		Pessoa pessoa = new Pessoa("Renan", 9, 7, 1990);
 		System.out.println(pessoa.getSigno());
 	}
 }
